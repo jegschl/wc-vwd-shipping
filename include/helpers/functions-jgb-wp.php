8e176b277acfdaffffad727038373176c8787dbd
Jorge Garrido <jegschl@gmail.com>
2023-02-28 11:44:13 -0300

    registro de logs

---
+++ include/helpers/functions-jgb-wp.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( ! defined( 'WPINC' ) ) {
+ 
+    die;
+ 
+}
+
+if (!function_exists('write_log')) {
+
+    function write_log($log) {
+        if (true === WP_DEBUG) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+
+}
