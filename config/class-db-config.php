b3dd9ff050b3450189a595c2c1a854fa3c6da6dd
Jorge Garrido <jegschl@gmail.com>
2023-03-12 10:53:39 -0300

    Delete & activation support on registers

--- config/class-db-config.php
+++ config/class-db-config.php
@@ -14,6 +14,8 @@ class JGBVWDSDbInitializator{
                     `desc` varchar(256) NOT NULL,
                     `parent` varchar(24) DEFAULT NULL,
                     `type` varchar(12) NOT NULL,
+                    `deleted` tinyint(1) NOT NULL DEFAULT '0',
+                    `active` tinyint(1) NOT NULL DEFAULT '1',
                     UNIQUE KEY `wp_wc_vwds_locations_id_IDX` (`id`) USING BTREE,
                     KEY `wp_wc_vwds_locations_location_code_IDX` (`location_code`) USING BTREE,
                     KEY `wp_wc_vwds_locations_parent_IDX` (`parent`) USING BTREE,
@@ -28,6 +30,8 @@ class JGBVWDSDbInitializator{
                     `unit_price` float NOT NULL,
                     `min_price` float DEFAULT NULL,
                     `zone_code` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+                    `deleted` tinyint(1) NOT NULL DEFAULT '0',
+                    `active` tinyint(1) NOT NULL DEFAULT '1',
                     UNIQUE KEY `wp_wc_vwds_rules_id_IDX` (`id`) USING BTREE,
                     KEY `wp_wc_vwds_rules_min_weight_IDX` (`min_weight`) USING BTREE,
                     KEY `wp_wc_vwds_rules_max_weight_IDX` (`max_weight`) USING BTREE,
@@ -39,6 +43,8 @@ class JGBVWDSDbInitializator{
                     `id` int unsigned NOT NULL AUTO_INCREMENT,
                     `code` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
                     `desc` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                    `deleted` tinyint(1) NOT NULL DEFAULT '0',
+                    `active` tinyint(1) NOT NULL DEFAULT '1',
                     UNIQUE KEY `wp_wc_vwds_zones_id_IDX` (`id`) USING BTREE,
                     UNIQUE KEY `wp_wc_vwds_zones_code_IDX` (`code`) USING BTREE
                   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
