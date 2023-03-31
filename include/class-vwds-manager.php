df2c50aee2957e4192da363b71a64365e7b906f3
Jorge Garrido <jegschl@gmail.com>
2023-03-31 20:08:40 -0300

    Retiro de acción con función inexistente

--- include/class-vwds-manager.php
+++ include/class-vwds-manager.php
@@ -169,7 +169,7 @@ class JGBVWDS_Manager{
 		add_action( 'JGB/VWDS/admin_settings_locations_before_datatable', [$this->adminMan,'locations_form_add_new_html_render']);
     
 		add_action('admin_enqueue_scripts',[$this->adminMan,'enqueue_js_zones']);
-		add_action('admin_enqueue_scripts',[$this->adminMan,'enqueue_css_zones']);
+		//add_action('admin_enqueue_scripts',[$this->adminMan,'enqueue_css_zones']);
 		add_action( 'JGB/VWDS/sections_zones', [$this->adminMan,'zones_adm_html_render']);
 		add_action( 'JGB/VWDS/admin_settings_zones_before_datatable', [$this->adminMan,'zones_price_mode_html_render']);
 		add_action( 'JGB/VWDS/admin_settings_zones_after_datatable', [$this->adminMan,'zones_price_importer_html_render']);
